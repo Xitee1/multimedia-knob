c4c2a0f888af6b6b09ab7cc172bf2e0ecc5a6a8f
Xitee <59659167+Xitee1@users.noreply.github.com>
2023-12-01 20:10:21 +0100

    General python code improvements & minor bug fixes

--- pico/code.py
+++ pico/code.py
@@ -11,27 +11,28 @@ from adafruit_hid.consumer_control_code import ConsumerControlCode
 
 print("== Pi Pico multifunction knob 2.0 ==")
 #
-# IMOPRTANT FOR EDITING
+# IMPORTANT FOR EDITING THIS SCRIPT
 # Press down the knob while plugging in to be able to edit the code.
 #
 # As an alternative you can type the following two commands:
 # import storage
 # storage.remount("/", readonly=False)
 #
+# Debug
 debug = False
 
+# Pins
 CLK_PIN = board.GP4
 DT_PIN = board.GP3
 SW_PIN = board.GP2
+
 rotateDelay = False
-count = 0
-totalMode = 1
+totalModes = 1
 currentMode = 0
 
 cc = ConsumerControl(usb_hid.devices)
-#mouse = Mouse(usb_hid.devices)
+# mouse = Mouse(usb_hid.devices)
 keyboard = Keyboard(usb_hid.devices)
-
 
 clk = digitalio.DigitalInOut(CLK_PIN)
 clk.direction = digitalio.Direction.INPUT
@@ -43,112 +44,112 @@ sw = digitalio.DigitalInOut(SW_PIN)
 sw.direction = digitalio.Direction.INPUT
 sw.pull = digitalio.Pull.UP
 
+
 def millis():
     return time.monotonic() * 1000
+
 
 def log(message):
     if debug:
         print(message)
 
+
 def ccw():
-    log("left")
-    
+    log("Knob: turned left")
+
     if currentMode == 0:
         # Volume decrement
         cc.send(ConsumerControlCode.VOLUME_DECREMENT)
-        
+
+
 def cw():
-    log("right")
-    
+    log("Knob: turned right")
+
     if currentMode == 0:
         # Volume increment
         cc.send(ConsumerControlCode.VOLUME_INCREMENT)
 
-        
 
-        
 def long_press():
-    #Mac sleep: CMD + OPT + EJECT
-    log("long-press")
-    
+    # Mac sleep: CMD + OPT + EJECT
+    log("Knob: long press detected")
+
+
 def short_press():
-    log("short-press")
+    log("Knob: short press detected")
     if currentMode == 0:
         cc.send(ConsumerControlCode.PLAY_PAUSE)
-    
-# Fix not working after wakeup (windows sleep)
-def resetKeyboard(force = False):
+
+
+def reset_keyboard(force=False):
     global keyboard, cc
-    
+
     if force:
         time.sleep(1)
         log("Resetting keyboard..")
         cc = ConsumerControl(usb_hid.devices)
         keyboard = Keyboard(usb_hid.devices)
     else:
-        if cc == None:
+        if cc is None:
             time.sleep(1)
             log("ConsumerControl not initialized. Trying again..")
             cc = ConsumerControl(usb_hid.devices)
-        if keyboard == None:
+        if keyboard is None:
             time.sleep(1)
             log("Keyboard not initialized. Trying again..")
             keyboard = Keyboard(usb_hid.devices)
-            
-    
+
+
 def loop():
     global rotateDelay
-    
-    currentState = clk.value
+
+    current_state = clk.value
     # Rotation
-    if currentState == False:
+    if not current_state:
         if not rotateDelay:
-            resetKeyboard()
+            reset_keyboard()
             try:
-                if dt.value != currentState:
+                if dt.value != current_state:
                     cw()
                 else:
                     ccw()
-            except Exception:
-                resetKeyboard(true)
-            
+            except Exception as e:
+                print("An error occurred: {}".format(e))
+                reset_keyboard(True)
+
             rotateDelay = True
     else:
         rotateDelay = False
-    
+
     # Press
     if sw.value == 0:
-        pressTime = millis()
+        press_time = millis()
         time.sleep(0.2)
-        longPress = False
-        resetKeyboard()
+        long_pressed = False
+        reset_keyboard()
         try:
-            while(sw.value == 0):
-                if(millis() - pressTime > 1000 and not longPress):
-                    longPress = True
+            while sw.value == 0:
+                if millis() - press_time > 1000 and not long_pressed:
+                    long_pressed = True
                     long_press()
-                
-            if(not longPress):
+
+            if not long_pressed:
                 short_press()
-        except Exception:
-            resetKeyboard(true)
+
+        except Exception as e:
+            print("An error occurred: {}".format(e))
+            reset_keyboard(True)
 
 
 if __name__ == "__main__":
     # Mount storage when knob is pressed while plugging in
-    if(sw.value == 0):
+    if sw.value == 0:
         storage.remount("/", readonly=False)
 
-        
-        
-    if debug:
-        while True:
+    while True:
+        # try except just in case there are any errors that might occur for any reason. Make sure it keeps running.
+        try:
             loop()
-    else:
-        while True:
-            try:
-                loop()
-            except Exception:
-                # log("An error occurred: {}".format(e))
-                resetKeyboard(true)
-
+        except Exception as e2:
+            print("An error in the loop occurred: {}".format(e2))
+            reset_keyboard(True)
